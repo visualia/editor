5e17781e274acd073b60324a10917bad2ddf25b1
kristjanjansen <kristjan.jansen@gmail.com>
2020-03-31 07:48:17 +0200

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+import { visualia } from " https://visualia.github.io/visualia/dist/visualia.js";
+import { VEditor } from "./VEditor.js";
+
+visualia({
+  components: { VEditor },
+  template: `<v-editor />`
+});
