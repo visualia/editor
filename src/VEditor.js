98116468ae1a03189d6a7c61ec4109ca52d0b384
kristjanjansen <kristjan.jansen@gmail.com>
2020-03-31 09:56:20 +0200

    save

--- src/VEditor.js
+++ src/VEditor.js
@@ -33,23 +33,23 @@ export const VEditor = {
   },
   template: `
   <div style="display: grid; grid-template-columns: 1fr 1fr; height: 100vh;">
-      <div style="display: flex; flex-direction: column;">
-        <v-save
-          :saveid="saveid"
-          :content="content"
-          :current-content="currentContent"
-          @load="onLoad"
-        />
-        <v-monaco
-          style="flex: 1;"
-          :content="currentContent"
-          @input:content="content => currentContent = content"
-        />
-      </div>
-      <v-content
-        style="overflow: auto; height: 100vh;"
-        :content="currentContent"
+    <div style="display: flex; flex-direction: column;">
+      <v-save
+        :saveid="saveid"
+        :content="content"
+        :current-content="currentContent"
+        @load="onLoad"
       />
+      <v-monaco
+        style="flex: 1;"
+        :content="currentContent"
+        @input:content="content => currentContent = content"
+      />
+    </div>
+    <v-content
+      style="overflow: auto; height: 100vh;"
+      :content="currentContent"
+    />
   </div>
   `
 };
