34ac60cb3e86da2e9ecbdfc38c23ea26364c1729
kristjanjansen <kristjan.jansen@gmail.com>
2020-04-07 21:26:26 +0200

    move editor

--- index.js
+++
@@ -1,7 +0,0 @@
-import { visualia } from " https://visualia.github.io/visualia/dist/visualia.js";
-import { VEditor } from "./src/VEditor.js";
-
-visualia({
-  components: { VEditor },
-  template: `<v-editor content="<v-slider />" />`
-});
